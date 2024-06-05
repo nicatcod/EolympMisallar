2b2b7fc24280e6eb4bda7c978d7788dfc8a17553
nicatcod <qaqa0qaqa002@gmail.com>
2024-06-05 17:51:13 +0400

    guzgu

--- src/Task1.java
+++ src/Task1.java
@@ -2,7 +2,7 @@ public class Task1 {
     public static void main(String[] args) {
         for (int i = 7; i >= 1; i--) {
 
-            for (int j=1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Task2 {
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // Boşluq
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // ulduz
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
