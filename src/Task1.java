11be8b1ef314d8d12351fd6881b679298ab58ca7
nicatcod <qaqa0qaqa002@gmail.com>
2024-06-05 17:00:49 +0400

    tersine

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Task1 {
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 7; i >= 1; i--) {
+
+            for (int j=1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
