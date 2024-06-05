9a77adfa18f298ab4f062f8fcaadc2a3aa1b45fc
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-06-05 15:47:29 +0400

    first commit

--- src/missal2.java
+++ src/missal2.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class missal2 {
-}
+public class missal2 {
+    public static void main(String[] args) {
+        //İlk 10 natural ədədin cəmini hesablamaq üçün proqram yazın.
+
+            int n = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                n = n + i;
+            }
+
+            System.out.print(n);
+
+        }
+    }
+
+
+
--- src/missal3.java
+++ src/missal3.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class missal3 {
+import java.util.Scanner;
+
+public class missal3 {
+    public static void main(String[] args) {
+        //İstifadəçiyə müsbət tam ədəd daxil etməyi təklif edən proqram yazın.
+        //Sonra həmin ədədin vurma cədvəlini çap etməlidir.
+        Scanner input = new Scanner(System.in);
+        System.out.println("Musbet Ededi daxil Ele");
+        int N = input.nextInt();
+        for (int i = 1; i <= N; i++) {
+             {
+
+
+                System.out.println("%d* %d=%d ",N,i, N*i);
+
+
+
+            }
+        }
+    }
 }
+
