d6ff4e04d7649e66c5a5e5305c6c48ebf4b9ada1
nicatcod <qaqa0qaqa002@gmail.com>
2024-06-12 01:54:01 +0400

    EolympMisallar

--- src/Eolymp14.java
+++ src/Eolymp14.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Eolymp14 {
+import java.util.Scanner;
+
+public class Eolymp14 {
+    public static void main(String[] args) {
+        //İki sıfır olmayan n və m tam ədədləri verilmişdir.
+        // n və m tam ədədləri eyni işarəlidirlərsə 1, əks halda 0 çap edin.
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int K = input.nextInt();
+        if(N==0||K==0){
+            System.out.println("Eded 0 olmaz");
+        }
+     else if (N > 0 && K > 0 || N < 0 && K < 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
 }
+
+
