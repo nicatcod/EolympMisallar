681278005d3aeff50a1d662da02713fd99e15026
nicatcod <qaqa0qaqa002@gmail.com>
2024-06-13 17:01:59 +0400

    EolympMisallayeni

--- src/Eolymp15.java
+++ src/Eolymp15.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Eolymp15 {
+import java.util.Scanner;
+
+public class Eolymp15 {
+    public static void main(String[] args) {
+        //Üç müxtəlif a,b,c ədədləri verilmişdir. Onlardan qiymətcə orta olanı tapın.
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if ((a > b && a < c) || (a > c && a < b)) {
+            System.out.println(a);
+        } else if ((b > a && b < c) || (b > c && b < a)) {
+            System.out.println(b);
+        } else {
+            System.out.println(c);
+        }
+    }
 }
+
