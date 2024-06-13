e9f14e23826fd02448935ce0fb0e7bbb141f3b05
nicatcod <qaqa0qaqa002@gmail.com>
2024-06-13 17:02:06 +0400

    EolympMisallayeni

--- src/Eolymp16.java
+++ src/Eolymp16.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Eolymp16 {
+import java.util.Scanner;
+
+public class Eolymp16 {
+    public static void main(String[] args) {
+        //Üçrəqəmli ədəd verilir. Onun rəqəmlərinin hasilini hesablamalı.
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int b =N/100;
+        int c =(N/10)%10;
+        int d =N%10;
+        int B = b*c*d;
+        System.out.println(B);
+    }
 }
